acce44ce4db793571e22523b1a1019b4473ec1ce
Chris Cruz <christopherc1331@gmail.com>
2020-05-31 19:45:26 -0700

    Finished ring buffer exercise

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,38 @@
+from collections import deque
+
+
 class RingBuffer:
+  
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity 
+        self.queue = deque()
+        self.length = 0
+        self.oldest = 0
 
     def append(self, item):
-        pass
+            if self.length + 1 <= self.capacity:
+                self.length += 1
+                self.oldest += 1
+                self.queue.append(item)
+            else:
+                if self.oldest + 1 <= self.capacity - 1:
+                    self.oldest += 1
+                else:
+                    self.oldest = 0
+                self.queue[self.oldest] = item
+            
+            
+
+
 
     def get(self):
-        pass
+        list = [x for x in self.queue if x is not None]
+        return list
+
+
+# test = RingBuffer(5)
+
+# for i in range(50):
+#     test.append(i)
+
+# print(test)
